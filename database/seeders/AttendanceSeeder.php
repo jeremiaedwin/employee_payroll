f1150a1984a45e0f5114485a872fc45259a7661b
jeremiaedwin <edwinjeremia00@gmail.com>
2025-01-20 14:26:48 +0700

    Update attendance seeder

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -14,10 +14,11 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
+        $status = ["leave", "absent"];
         for($i = 1; $i <= 50; $i++){
-            $currentDate = date("Y-m-d");
-            $presentTime = $this->generateRandomTime('07:30', '08:30');
-            $leaveTime = $this->generateRandomTime('16:30', '17:00');
+            $currentDate = "2025-01-23";
+            $presentTime = $this->generateRandomTime('07:30', '08:30', $currentDate);
+            $leaveTime = $this->generateRandomTime('16:30', '17:00', $currentDate);
 
             // Convert strings to Carbon instances
             $present = Carbon::parse($presentTime);
@@ -26,21 +27,39 @@ class AttendanceSeeder extends Seeder
             // Calculate the difference in hours
             $workingHours = $present->floatDiffInHours($leave);
 
-            Attendance::create([
-                "employee_id" => $i,
-                "date" => $currentDate,
-                "working_hours" => $workingHours,
-                "status" => "leave",
-                "created_at" => $presentTime,
-                "updated_at" => $leaveTime
-            ]);
+            $attendanceStatus = $status[rand(0,1)];
+
+            if($attendanceStatus == "absent") {
+                Attendance::create([
+                    "employee_id" => $i,
+                    "date" => $currentDate,
+                    "working_hours" => 0,
+                    "status" => $attendanceStatus,
+                ]);
+            } else {
+                Attendance::create([
+                    "employee_id" => $i,
+                    "date" => $currentDate,
+                    "working_hours" => $workingHours,
+                    "status" => $attendanceStatus,
+                    "created_at" => $presentTime,
+                    "updated_at" => $leaveTime
+                ]);
+            }
+
+            // echo "Date = " . $currentDate ."\n";
+            // echo "Present = " . $presentTime ."\n";
+            // echo "Leave = " . $leaveTime ."\n";
+            // echo "Working Hours = " . $workingHours ."\n\n";
+
+            
         }
     }
 
-    private function generateRandomTime($startTime, $endTime)
+    private function generateRandomTime($startTime, $endTime, $date)
     {
-        $start = Carbon::createFromFormat('H:i', $startTime);
-        $end = Carbon::createFromFormat('H:i', $endTime);
+        $start = Carbon::createFromFormat('Y-m-d H:i', "$date $startTime");
+        $end = Carbon::createFromFormat('Y-m-d H:i', "$date $endTime");
 
         $randomTime = Carbon::createFromTimestamp(rand($start->timestamp, $end->timestamp));
         return $randomTime->format('Y-m-d H:i:s');
